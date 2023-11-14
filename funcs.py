36a103df1211998d0d945e4307f20eccbcc01c79
Антон Федяев <fearthe54@gmail.com>
2023-11-14 12:51:10 +0300

    update files

--- funcs.py
+++ funcs.py
@@ -1,5 +1,5 @@
 def Addition(a, b):
-    return a + b
+    return print(a + b)
 
 def Subtract(a, b):
-    return a - b
+    return print(a - b)
---
+++ main.py
@@ -0,0 +1,5 @@
+import funcs
+
+funcs.Addition(5, 7)
+
+funcs.Subtract(8, 3)
